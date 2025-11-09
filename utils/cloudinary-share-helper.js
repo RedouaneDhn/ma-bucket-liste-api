a5ed51cac160484513d9e494db80a4b3f3452d48
RedouaneDhn <r.dahnoun@yahoo.fr>
2025-11-09 12:09:24 +0100

    fix: ajouter config Cloudinary + corriger structure données + séparer overlays

--- utils/cloudinary-share-helper.js
+++ utils/cloudinary-share-helper.js
@@ -5,6 +5,13 @@
 
 const cloudinary = require('cloudinary').v2;
 
+// Configuration Cloudinary (depuis variables d'environnement)
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 // Configuration des formats par réseau social
 const SOCIAL_FORMATS = {
   instagram: {
